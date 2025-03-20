8dd9a8c88ed137b7bf5c51faa3173866eb182d45
Wataru Ashihara <wataash0607@gmail.com>
2025-03-20 22:56:51 +0900

    Switch to ESM

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@
 // SPDX-FileCopyrightText: Copyright (c) 2021-2025 Wataru Ashihara <wataash0607@gmail.com>
 // SPDX-License-Identifier: Apache-2.0
 
+import esMain from "es-main";
+
 // API
-export type { Jisseki, Kinmu, Kousu, ProjectName } from "./cli";
-export { VERSION } from "./cli";
+export type { Jisseki, Kinmu, Kousu, ProjectName } from "./cli.js";
+export { VERSION } from "./cli.js";
 
-import { cliMain } from "./cli";
+import { cliMain } from "./cli.js";
 
-if (require.main === module) {
-  cliMain();
+if (esMain(import.meta)) {
+  await cliMain();
 }
